f85c535f2c9e0aa338a963a64748505811f15cab
alx <agarkov.aleksei1@yandex.ru>
2026-03-27 02:58:52 +0300

    Invert assert order according to API

--- tests/core/test_config.py
+++ tests/core/test_config.py
@@ -7,17 +7,17 @@ from app.core.config import Settings
 class TestSettings(TestCase):
     def test_settings(self):
         test_settings = Settings(_env_file=".env.example")
-        self.assertEqual(first=test_settings.APP_NAME, second="llm-p")
-        self.assertEqual(first=test_settings.ENV, second="local")
+        self.assertEqual("llm-p", test_settings.APP_NAME)
+        self.assertEqual("local", test_settings.ENV)
 
-        self.assertEqual(first=test_settings.JWT_SECRET, second="change_me_super_secret")
-        self.assertEqual(first=test_settings.JWT_ALG, second="HS256")
-        self.assertEqual(first=test_settings.ACCESS_TOKEN_EXPIRE_MINUTES, second=60)
+        self.assertEqual("change_me_super_secret", test_settings.JWT_SECRET)
+        self.assertEqual("HS256", test_settings.JWT_ALG)
+        self.assertEqual(60, test_settings.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-        self.assertEqual(first=test_settings.SQLITE_PATH, second=Path("./app.db"))
+        self.assertEqual(Path("./app.db"), test_settings.SQLITE_PATH)
 
-        self.assertEqual(first=test_settings.OPENROUTER_API_KEY, second="change_me_openrouter_api_key")
-        self.assertEqual(first=test_settings.OPENROUTER_BASE_URL, second="https://openrouter.ai/api/v1")
-        self.assertEqual(first=test_settings.OPENROUTER_MODEL, second="stepfun/step-3.5-flash:free")
-        self.assertEqual(first=test_settings.OPENROUTER_SITE_URL, second="https://example.com")
-        self.assertEqual(first=test_settings.OPENROUTER_APP_NAME, second="llm-fastapi-openrouter")
+        self.assertEqual("change_me_openrouter_api_key", test_settings.OPENROUTER_API_KEY)
+        self.assertEqual("https://openrouter.ai/api/v1", test_settings.OPENROUTER_BASE_URL)
+        self.assertEqual("stepfun/step-3.5-flash:free", test_settings.OPENROUTER_MODEL)
+        self.assertEqual("https://example.com", test_settings.OPENROUTER_SITE_URL)
+        self.assertEqual("llm-fastapi-openrouter", test_settings.OPENROUTER_APP_NAME)
