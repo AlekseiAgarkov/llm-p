9bb8ca430fac8103c3ce180198ca5b6f1783b512
alx <agarkov.aleksei1@yandex.ru>
2026-04-13 23:36:01 +0300

    Implement security.py

---
+++ app/core/security.py
@@ -0,0 +1,101 @@
+import uuid
+from dataclasses import dataclass
+from datetime import datetime, UTC
+from enum import Enum
+from typing import Tuple, Dict, Any
+
+import bcrypt
+from fastapi.security import HTTPBasic
+from jose import jwt
+from passlib.context import CryptContext
+
+security = HTTPBasic()
+
+
+@dataclass
+class SilenceBcryptWarningBug:
+    """
+    __about__ в bcrypt сломан для версий после 4.0.1. Описано тут:
+        - https://foss.heptapod.net/python-libs/passlib/-/work_items/190
+        - https://stackoverflow.com/questions/79469355/error-attributeerror-problem-with-passlibbcrypt-module
+
+    Как чинить предложено тут:
+        https://github.com/pyca/bcrypt/issues/684#issuecomment-2430047176
+
+    Добавил, чтобы:
+     - bcrypt не ругался на __about__;
+     - не downgrade'ить;
+     - соблюсти условие задания;
+     - можно было ловить ошибки.
+    """
+    __version__: str = getattr(bcrypt, "__version__")
+
+
+setattr(bcrypt, "__about__", SilenceBcryptWarningBug())
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+__all__ = ["security",
+           "hash_password",
+           "verify_password",
+           "create_access_token",
+           "create_refresh_token",
+           "decode_token"]
+
+
+class JWTTokenType(Enum):
+    refresh = "refresh"
+    access = "access"
+
+
+@dataclass(frozen=True)
+class JWTAccessToken:
+    sub: str
+    type: str
+    iat: int
+    exp: int
+
+
+@dataclass(frozen=True)
+class JWTRefreshToken:
+    sub: str
+    type: str
+    jti: str
+    iat: int
+    exp: int
+
+
+def _now() -> int:
+    return int(datetime.now(UTC).timestamp())
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+
+def verify_password(plain_password: str, password_hash: str) -> bool:
+    return pwd_context.verify(plain_password, password_hash)
+
+
+def create_access_token(sub: str, jwt_secret: str, jwt_algorithm: str, token_expire_minutes: int) -> str:
+    now: int = _now()
+    payload: dict = JWTAccessToken(sub=sub,
+                                   type=JWTTokenType.access.name,
+                                   iat=now,
+                                   exp=now + token_expire_minutes).__dict__
+    return jwt.encode(payload, jwt_secret, algorithm=jwt_algorithm)
+
+
+def create_refresh_token(sub: str, jwt_secret: str, jwt_algorithm: str, token_expire_minutes: int) -> Tuple[str, str]:
+    now: int = _now()
+    payload: dict = JWTRefreshToken(sub=sub,
+                                    type=JWTTokenType.refresh.name,
+                                    jti=uuid.uuid4().hex,
+                                    iat=now,
+                                    exp=now + token_expire_minutes).__dict__
+    token = jwt.encode(payload, jwt_secret, algorithm=jwt_algorithm)
+    return token, payload['jti']
+
+
+def decode_token(token: str, jwt_secret: str, jwt_algorithm: str) -> Dict[str, Any]:
+    return jwt.decode(token, jwt_secret, algorithms=[jwt_algorithm])
---
+++ tests/api/test_security.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+
+from app.core.config import Settings
+from app.core.security import verify_password, create_access_token, decode_token, JWTTokenType, create_refresh_token
+
+
+class TestSecurity(TestCase):
+    def setUp(self):
+        self.settings = Settings(_env_file=".env.example")
+
+        self.sub = "sub"
+        self.jwt_secret = self.settings.JWT_SECRET
+        self.jwt_algorithm = self.settings.JWT_ALG
+        self.jwt_expire_minutes = self.settings.ACCESS_TOKEN_EXPIRE_MINUTES
+
+    def tearDown(self):
+        del self.settings
+
+    def test_verify_password(self):
+        self.assertTrue(verify_password(plain_password="password",
+                                        password_hash="$2b$12$egYdvOrgav9UfJ79ei2T2.gIAXIo0Rkmj9iO65VTzsunvToFjFaMy"))
+
+    def test_decode_access_token(self):
+        token = create_access_token(
+            sub=self.sub,
+            jwt_secret=self.jwt_secret,
+            jwt_algorithm=self.jwt_algorithm,
+            token_expire_minutes=self.jwt_expire_minutes)
+
+        decoded_token = decode_token(token=token,
+                                     jwt_secret=self.jwt_secret,
+                                     jwt_algorithm=self.jwt_algorithm)
+
+        self.assertEqual(self.sub, decoded_token["sub"])
+        self.assertEqual(JWTTokenType.access.name, decoded_token["type"])
+
+    def test_decode_refresh_token(self):
+        token, _ = create_refresh_token(
+            sub=self.sub,
+            jwt_secret=self.jwt_secret,
+            jwt_algorithm=self.jwt_algorithm,
+            token_expire_minutes=self.jwt_expire_minutes)
+
+        decoded_token = decode_token(token=token,
+                                     jwt_secret=self.jwt_secret,
+                                     jwt_algorithm=self.jwt_algorithm)
+
+        self.assertEqual(self.sub, decoded_token["sub"])
+        self.assertEqual(JWTTokenType.refresh.name, decoded_token["type"])
